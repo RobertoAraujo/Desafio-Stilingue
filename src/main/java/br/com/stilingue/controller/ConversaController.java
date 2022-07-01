af875a1f5075d81223cc2ce20e6dd9ea82f26c22
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-01 16:40:44 -0500

    criação do endpoint listar

--- src/main/java/br/com/stilingue/controller/ConversaController.java
+++ src/main/java/br/com/stilingue/controller/ConversaController.java
@@ -1,16 +1,27 @@
 package br.com.stilingue.controller;
 
+import br.com.stilingue.dto.ConversaDTO;
+import br.com.stilingue.model.ConversaEntity;
+import br.com.stilingue.service.ConversaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
-@RequestMapping("/v1/estatistica")
+@RequestMapping("/v1/conversa/")
 public class ConversaController {
 
-    @GetMapping
-    public void listaTodas(){
+    @Autowired
+    private ConversaService conversaService;
 
+    @GetMapping(value = "listar")
+    public ResponseEntity<List<ConversaEntity>> listarTodos(){
+        return ResponseEntity.ok(conversaService.findAll());
     }
 
 

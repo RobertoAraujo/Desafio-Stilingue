520043c4629814677bebdc3915e92ca068a9852e
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-01 13:00:03 -0500

    modificação do nome conversa  para  ConversaEntity

--- src/main/java/br/com/stilingue/repository/ConversaRepository.java
+++ src/main/java/br/com/stilingue/repository/ConversaRepository.java
@@ -1,9 +1,9 @@
 package br.com.stilingue.repository;
 
-import br.com.stilingue.model.Conversa;
+import br.com.stilingue.model.ConversaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConversaRepository extends JpaRepository<Conversa, Long> {
+public interface ConversaRepository extends JpaRepository<ConversaEntity, Long> {
 }

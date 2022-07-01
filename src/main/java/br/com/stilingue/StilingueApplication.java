c8f4f127e0e939a21346eca1f21d25ee6f72e934
Roberto A  Moraes <endriosrobert@gmail.com>
2022-06-30 19:55:03 -0500

    Primeiro commit

---
+++ src/main/java/br/com/stilingue/StilingueApplication.java
@@ -0,0 +1,13 @@
+package br.com.stilingue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StilingueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StilingueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stilingue/StilingueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stilingue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StilingueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

310d74ea21ad873d0b8058ec6a54d525eb5fa67f
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-01 09:03:37 -0500

    criação do repository da classe coversa

--- src/main/java/br/com/stilingue/repository/ConversaRepository.java
+++ src/main/java/br/com/stilingue/repository/ConversaRepository.java
@@ -1,2 +1,9 @@
-package br.com.stilingue.repository;public interface ConversaRepository {
+package br.com.stilingue.repository;
+
+import br.com.stilingue.model.Conversa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ConversaRepository extends JpaRepository<Conversa, Long> {
 }

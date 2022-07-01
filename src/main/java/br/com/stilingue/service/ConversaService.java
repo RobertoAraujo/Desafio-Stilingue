9f9ed5d9b656d7621a760d5aa1b7e4739898c566
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-01 16:38:49 -0500

    criação da classe service para regas de negocio methodo lista

--- src/main/java/br/com/stilingue/service/ConversaService.java
+++ src/main/java/br/com/stilingue/service/ConversaService.java
@@ -1,2 +1,19 @@
-package br.com.stilingue.service;public class ConversaService {
+package br.com.stilingue.service;
+
+import br.com.stilingue.model.ConversaEntity;
+import br.com.stilingue.repository.ConversaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConversaService {
+
+    @Autowired
+    private ConversaRepository conversaRepository;
+
+    public List<ConversaEntity> findAll() {
+        return conversaRepository.findAll();
+    }
 }

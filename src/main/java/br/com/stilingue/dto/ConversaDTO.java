0bd78ef332c9255fd3fee443c3280e22877ec875
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-03 17:47:40 -0500

    possivle mudança na logica com dto e mapper

--- src/main/java/br/com/stilingue/dto/ConversaDTO.java
+++ src/main/java/br/com/stilingue/dto/ConversaDTO.java
@@ -1,2 +1,70 @@
-package br.com.stilingue.dto;public class ConversaDTO {
+package br.com.stilingue.dto;
+
+import br.com.stilingue.constants.Status;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class ConversaDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private LocalDate data;
+    private String mensagem;
+    private String status;
+
+    public ConversaDTO(LocalDate data, String mensagem, String status) {
+        this.data = data;
+        this.mensagem = mensagem;
+        this.status = status;
+    }
+
+    public ConversaDTO() {
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ConversaDTO that = (ConversaDTO) o;
+        return Objects.equals(data, that.data) && Objects.equals(mensagem, that.mensagem) && Objects.equals(status, that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, mensagem, status);
+    }
+
+    @Override
+    public String toString() {
+        return "ConversaDTO{" +
+                "data=" + data +
+                ", mensagem='" + mensagem + '\'' +
+                ", status='" + status + '\'' +
+                '}';
+    }
 }
--- src/main/java/br/com/stilingue/mapper/ConversaMapper.java
+++ src/main/java/br/com/stilingue/mapper/ConversaMapper.java
@@ -1,2 +1,29 @@
-package br.com.stilingue.mapper;public class ConversaMapper {
+package br.com.stilingue.mapper;
+
+import br.com.stilingue.dto.ConversaDTO;
+import br.com.stilingue.model.ConversaEntity;
+
+public class ConversaMapper {
+
+    public static ConversaEntity toEntity(ConversaDTO dto){
+        return mergeEntity(new ConversaEntity(), dto);
+    }
+
+    private static ConversaEntity mergeEntity(ConversaEntity entity, ConversaDTO dto) {
+        entity.setData(dto.getData());
+        entity.setMensagem(dto.getMensagem());
+        entity.setStatus(dto.getStatus());
+        return entity;
+    }
+
+    public static ConversaDTO toDTO(ConversaEntity entity){
+        return merge(new ConversaDTO(), entity);
+    }
+    private static ConversaDTO merge(ConversaDTO dto,ConversaEntity entity){
+        dto.setData(entity.getData());
+        dto.setMensagem(entity.getMensagem());
+        dto.setStatus(entity.getStatus());
+        return dto;
+    }
+
 }

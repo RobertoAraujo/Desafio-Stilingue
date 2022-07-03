d0982e5c1908fe8b3392019af0f4a48d481efba7
Roberto A  Moraes <endriosrobert@gmail.com>
2022-07-03 16:44:39 -0500

    Methodo de put e delet

--- src/main/java/br/com/stilingue/controller/ConversaController.java
+++ src/main/java/br/com/stilingue/controller/ConversaController.java
@@ -3,26 +3,49 @@ package br.com.stilingue.controller;
 import br.com.stilingue.dto.ConversaDTO;
 import br.com.stilingue.model.ConversaEntity;
 import br.com.stilingue.service.ConversaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/conversa/")
 public class ConversaController {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConversaController.class);
     @Autowired
     private ConversaService conversaService;
 
     @GetMapping(value = "listar")
-    public ResponseEntity<List<ConversaEntity>> listarTodos(){
+    public ResponseEntity<List<ConversaEntity>> listar(){
+        LOGGER.info("Buscando Conversas.");
         return ResponseEntity.ok(conversaService.findAll());
     }
-
-
+    @GetMapping(value = "{id}")
+    public Optional<ConversaEntity> getById(@PathVariable long id) {
+        LOGGER.info("Buscando Conversas por Id.");
+        return conversaService.findById(id);
+    }
+    @PostMapping(value = "inserir")
+    public ConversaEntity create(@RequestBody ConversaEntity entity) {
+        LOGGER.info("Cadastrado com sucesso!");
+        return conversaService.save(entity);
+    }
+    @PutMapping(value = "update/{id}")
+    public ConversaEntity update(@PathVariable String id,@RequestBody ConversaEntity conversa) {
+        return this.conversaService.updateConversa(Long.parseLong(id),conversa);
+    }
+    @DeleteMapping(value = "delete/{id}")
+    public List<ConversaEntity> deleteCourse(@PathVariable String id) {
+        if (id != null){
+            LOGGER.info("Buscando Conversas por Id.");
+            return this.conversaService.deleteConversa(Long.parseLong(id));
+        }
+        LOGGER.error("Fala ao busca o Id.");
+        return null;
+    }
 }
